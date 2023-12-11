2ec34dfe1f477b10a979bea9e31c7ce830098dd9
Allison Sheridan <allison@podfeet.com>
2023-12-10 16:48:14 -0800

    backup of updated scripts
    At the very lease I know in ocrpdf.sh I quoted my variables.

--- zsh-bash-scripts/ocrpdf.sh
+++ zsh-bash-scripts/ocrpdf.sh
@@ -8,11 +8,11 @@ inputName=$1
 echo "inputName is $inputName"
 
 # put the path into a variable
-dirName=$(dirname $inputName)
+dirName=$(dirname "$inputName")
 echo "dirName is $dirName"
 
 # Strip the path and file extension and assign to a variable
-inputBaseName=$(basename $inputName .pdf)
+inputBaseName=$(basename "$inputName" .pdf)
 echo "inputBaseName is $inputBaseName"
 
 # Create a variable to add the text "-OCR" and the file extension .pdf
@@ -20,7 +20,7 @@ add="-OCR.pdf"
 echo "add is $add"
 
 # Create a variable for the output file name that concatenates the base name of the input and adds -0CR and the file extension .pdf
-outputName=${dirName}/${inputBaseName}${add}
+outputName="${dirName}/${inputBaseName}${add}"
 echo "outputName is $outputName"
 
 # Send a dialog box to the user to tell them it's working
@@ -29,4 +29,4 @@ osascript -e 'display dialog "I am working on it, gimme a sec!"'
 # Run ocrmypdf on the original input file name, set the output to pdf and save to the new inputName with -OG and .pdf
 # As I'm going to use this shell script inside a Keyboard Maestro macro, I have to specify the full path (KBM doesn't know the path)
 
-ocrmypdf --output-type pdf $inputName $outputName
+ocrmypdf --output-type pdf "$inputName" "$outputName"
