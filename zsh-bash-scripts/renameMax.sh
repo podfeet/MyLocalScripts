9305077de1df9aa3f2eba0f3f58df1cb75a78b9d
Allison Sheridan <allison@podfeet.com>
2024-05-21 17:16:58 -0700

    Feat: create shell script to rename Max to remove incremented number

---
+++ zsh-bash-scripts/renameMax.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# reset M3 Max LocalHostName without incrementing number
+sudo scutil --set LocalHostName Al-M3-Max
+
+# reset M3 Max ComputerName without incrementing number
+sudo scutil --set ComputerName Al-M3-Max
